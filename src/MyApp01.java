e0553210843868cc55fe208ebbfdbd5308f4b221
Daos <sadom664@gmail.com>
2016-03-29 08:59:44 +0600

    new changes

--- src/MyApp01.java
+++ src/MyApp01.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.handler.ClientSOAPHandlerTube;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,14 +8,16 @@ public class MyApp01 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите последовательность чисел или Y для завершения:");
+
         for( ; ; )
         {
             String str = reader.readLine();
             int num = Integer.parseInt(str);
+            System.out.println(num);
+
         }
 
 
-//        System.out.println(num);
         /*ArithmeticOperations<Integer> arithmeticOperations = new ArithmeticOperations();
         Integer[] a = {1,2,3,4};
         Integer b = arithmeticOperations.getNumberSum(a);
